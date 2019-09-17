1e30df80081073b9e379a027160d7e7600e620bb
KirillNitchenko <kirill.nitchenko@asferro.com>
2019-09-17 13:08:03 +0300

    add fixtures pages

--- conftest.py
+++ conftest.py
@@ -1,14 +1,23 @@
 import pytest
-from driver import WebDriver
 from selenium import webdriver
+from pages.notes_page import Notes_Page
 
-@pytest.yield_fixture(scope='session')
-def invoke():
+
+@pytest.fixture(scope='class')
+def invoke(request):
     options = webdriver.ChromeOptions()
     options.add_experimental_option('w3c', False)
     _driver = webdriver.Chrome(options=options)
+    request.cls._driver = _driver
+    yield
+    _driver.close()
 
-    return _driver
+@pytest.mark.usefixtures('invoke')
+@pytest.fixture(scope='class')
+def init_notes_page(request):
+    notespage = Notes_Page(request.cls._driver)
+    request.cls.notespage = notespage
+    yield
 
 # @pytest.fixture(scope='session', autouse=True)
 # def close(request, driver):
--- test_in/test_notes.py
+++ test_in/test_notes.py
@@ -1,12 +1,16 @@
-from pages import notes_page
+import pytest
 
+@pytest.mark.usefixtures('init_notes_page')
+@pytest.mark.usefixtures('invoke')
 class Test_Notes():
 
-    def test_notes_add(self, invoke):
-        notespage = notes_page.Notes_Page(invoke)
-        notespage.go_to_home()
-        notespage.navigate_to_notes()
-        notespage.open_add_notes()
-        notespage.fill_notes_info("new name", "new short body text")
-        notespage.save_modal()
-        
+    def test_notes_add(self):
+        self.notespage.go_to_home()
+        self.notespage.navigate_to_notes()
+        self.notespage.open_add_notes()
+        self.notespage.fill_notes_info("new name", "new short body text")
+        self.notespage.save_modal()
+
+
+    def test_wait(self):
+        pass
